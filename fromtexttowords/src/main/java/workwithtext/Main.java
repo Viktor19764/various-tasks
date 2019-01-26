39236e0e138dd8f609221a2d8b24bec8ed8f9823
viktor19764 <czaass@gmail.com>
2019-01-26 08:48:11 +0200

    other changes

--- fromtexttowords/src/main/java/workwithtext/Main.java
+++ fromtexttowords/src/main/java/workwithtext/Main.java
@@ -1,8 +1,11 @@
 package workwithtext;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+
         FromTextToWords fromTextToWord = new FromTextToWords(args[0]);
         fromTextToWord.fromTextToWords();
 
