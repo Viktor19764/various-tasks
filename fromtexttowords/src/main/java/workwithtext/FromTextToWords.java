0758f9128ad0027b089c6fd5247fc471e4b51252
viktor19764 <czaass@gmail.com>
2019-03-05 11:12:09 +0200

    fixed

--- fromtexttowords/src/main/java/workwithtext/FromTextToWords.java
+++ fromtexttowords/src/main/java/workwithtext/FromTextToWords.java
@@ -1,6 +1,8 @@
 package workwithtext;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
@@ -45,13 +47,26 @@ public class FromTextToWords {
         }
 
         //from stringBuilder to words
-        String[] punctuationMarks = {",", ".", "!", "?", "-", ":", "\"", "(", ")", "—", "/", ";", ls, "\n", "\r\n"};
+        String[] punctuationMarks = {"®","\t","…","$","–", "+", "_", "@", "&", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "©", "“", "\\", "%", "{", "}", "[", "]", ">", "<", "=", "#", ",", ".", "!", "?", "-", ":", "\"", "(", ")", "—", "/", ";", ls, "\n", "\r\n"};
         for (int i = 0; i < stringBuilder.length(); i++) {
+            //change all words to lower case
+            char c = stringBuilder.charAt(i);
+            stringBuilder.setCharAt(i, Character.toLowerCase(c));
+            //remove punctuation
             if (Arrays.asList(punctuationMarks).contains(Character.toString(stringBuilder.charAt(i)))) {
                 stringBuilder.replace(i, i + 1, " ");
             }
+
             if ((stringBuilder.charAt(i) == ' ' || stringBuilder.charAt(i) == ' ') && i < stringBuilder.length() - 3) {
-                stringBuilder.replace(i + 1, i + 2, Character.toString(stringBuilder.charAt(i + 1)).toUpperCase());
+                //replace "'" on start of word
+                if (stringBuilder.charAt(i + 1) == '\'') {
+                    stringBuilder.replace(i + 1, i + 2, " ");
+                }
+                //replace "'" on end of word
+                if (i > 0 && stringBuilder.charAt(i - 1) == '\'') {
+                    stringBuilder.replace(i - 1, i, " ");
+                }
+                //stringBuilder.replace(i + 1, i + 2, Character.toString(stringBuilder.charAt(i + 1)).toUpperCase());
                 //stringBuilder.replace(i, i + 1, "-");
                 //stringBuilder.replace(i+1, i+2, "---");
             }
@@ -60,27 +75,31 @@ public class FromTextToWords {
 
         String[] wholeStringArray = stringBuilder.toString().split(" ", 0);//нерозривний пробіл
         Set<String> words = new TreeSet();
-        Set<String> wordsForTranscription = new TreeSet();
         String[] stringArray2 = null;
         int i = 0;
+        //набирання множини слів
         for (String el : wholeStringArray) {
             i = 0;
             stringArray2 = el.split(" ", 0);//звичайний пробіл
             words.addAll(Arrays.asList(stringArray2));//множини без кавичок
-            for (String el2 : stringArray2) {
-                stringArray2[i] = "\"" + stringArray2[i] + "\"";//додавання кавичок до кожного слова
-                i++;
-            }
-            wordsForTranscription.addAll(Arrays.asList(stringArray2));
         }
 
         words.remove("");  //delete empty strings
         words.remove("\"");  //delete empty strings
-        wordsForTranscription.remove("\"");  //delete empty strings
-        wordsForTranscription.remove("\"\"");  //delete empty strings
-        wordsForTranscription.remove("\"\"\"");  //delete empty strings
+
+        //add commas quotes
+        Set<String> wordsWithCommas = new TreeSet();
+        Set<String> wordsForTranscription = new TreeSet();
+        for(String el: words){
+            if(el.length()>1) {
+                wordsWithCommas.add(el + ",");
+                wordsForTranscription.add("\"" + el + "\"");
+            }
+        }
+
+
         //trim words
-        for (String el : words) {
+        for (String el : wordsWithCommas) {
             el.trim();
         }
         for (String el : wordsForTranscription) {
@@ -90,8 +109,15 @@ public class FromTextToWords {
         //System.out.println(((TreeSet<String>) wordsForTranscription).first());
 
         //words to file write
-        new WriteReadFiles().writeFile(words, "words.txt");
+        new WriteReadFiles().writeFile(wordsWithCommas, "words.txt");
         new WriteReadFiles().writeFile(wordsForTranscription, "wordsfortranscription.txt");
+        //empty file for translation
+        try {
+            new BufferedWriter(new FileWriter("onlyTranslation.txt")).close();
+            new BufferedWriter(new FileWriter("transcription.txt")).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
 
     }
--- fromtexttowords/src/main/java/workwithtext/Main.java
+++ fromtexttowords/src/main/java/workwithtext/Main.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+        FromTextToWords fromTextToWord = null;
+        ToCsv toCsv = null;
 
-        FromTextToWords fromTextToWord = new FromTextToWords(args[0]);
-        fromTextToWord.fromTextToWords();
-
+        //if(args.length>0 && args[0]=="input.txt") {
+        if(args.length>0) {
+            fromTextToWord = new FromTextToWords(args[0]);
+            fromTextToWord.fromTextToWords();
+        }
+        else
+            System.out.println("Any input.txt");
     }
-
 }
